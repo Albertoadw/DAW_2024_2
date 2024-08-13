b0e1e81917e2a826259a7cd7f112b9148b29604f
Alberto <lff9567@gmail>
2024-08-13 14:53:57 -0300

    Adicionei uma linha

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express'
 const app = express();
 
-app.get('/',function (req,res){
+app.get('/',function(req,res){
     res.send("Oi Mundo!")
 })
 
-app.get('/Alberto',function (req,res){
-     res.send("Oi Diego!")
+app.get('/Alberto',function(req,res){
+     res.send("Alberto!")
 })
+
+app.get('/:x', function(req,res){
+    const mensagem = "Oi"+req.parans.x; 
+    res.send()
+})
+
+app.get('/:nome/:sobrenome',function(res,res){
+    const mensagem = "Oi"+req;parans.nome+""+req
+    res.send(mensagem)
+})
+
 
 app.listen(3000)
